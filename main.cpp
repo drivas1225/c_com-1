87abb6345d161c9b8a2da8d74eff3212755cfce0
drivas1225 <diego.rivas@ucsp.edu.pe>
2017-04-17 21:50:34 -0500

    tarea

--- main.cpp
+++ main.cpp
@@ -57,41 +57,44 @@ bool es_primo (int x){
     return res;
 }
 
-int sum(const int arr[], const int tam){
+int sum(const int * arr, const int tam){
     int suma = 0;
     for(int i =0 ; i < tam ; i++ ){
-        suma = suma + arr[i];
+        suma = suma + *arr;
+        arr++;
     }
 
     return suma;
 }
 
 
-float prom(const int arr[], const int tam){
+float prom(const int * arr, const float tam){
 
     return (float)sum(arr,tam)/tam;
 }
 
-void invert(int arr[], const int tam){
+void invert(int * arr, const int tam){
 
-    int p;
+    int temp;
     for(int i =0 ; i < (tam/2);i++){
-        p=arr[i];
-        arr[i]=arr[tam-1-i];
-        arr[tam-1-i] = p;
+      temp = *(arr+i);
+      *(arr+i) = *(arr+tam-1-i);
+      *(arr+tam-1-i) = temp;
+
+
     }
 
 }
 
-void ord_arr(int arr[], const int tam){
+void ord_arr(int * arr, const int tam){
 
      for(int i = 0 ; i<tam;i++){
          for(int j = i; j<tam;j++){
-            if(arr[j]<arr[i]){
+            if(*(arr+j)<*(arr+i)){
                 int tem;
-                tem = arr[i];
-                arr[i]=arr[j];
-                arr[j]=tem;
+                tem = *(arr+i);
+                *(arr+i)=*(arr+j);
+                *(arr+j)=tem;
             }
 
          }
@@ -100,10 +103,10 @@ void ord_arr(int arr[], const int tam){
 
 }
 
-int tam_a(char arr[]){
+int tam_a(char * arr){
 
     int tam=0;
-    for( int i =0 ; arr[i] != '\0';i++){
+    for( int i =0 ; *arr != '\0';i++,arr++){
         tam=tam+1;
 
     }
@@ -111,15 +114,124 @@ int tam_a(char arr[]){
 
 }
 
+void imprimir_m(int ** mat , int filas , int columnas){
+    for (int i = 0 ; i<filas;i++){
+        for (int j = 0 ; j < columnas;j++){
+            cout<< mat[i][j] <<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+
+}
+
+void mul_mat(int ** mat1, int ** mat2,int fil1,int col1,int fil2,int col2){
+
+    if (col1 != fil2)
+        cout<<"no se puede hacer la multiplicacion :(";
+        return ;
+
+    int ret[fil1][col2];
+    for (int i = 0 ;i < fil1;i++){
+        for (int j = 0; j < col2;j++){
+            ret[i][j]=0;
+            for(int k = 0 ; k < col1; k++){
+                ret[i][j]=ret[i][j]+(mat1[i][k]*mat2[k][j]);
+
+            }
+
+        }
+
+
+    }
+
+     for (int i = 0 ; i<fil1;i++){
+        for (int j = 0 ; j < col2;j++){
+            cout<< ret[i][j] <<" ";
+        }
+        cout<<endl;
+
+     }
+
+    return ;
+}
+
+
 
 
 
 int main()
 {
 
-    char arr[] = "hello";
-    cout<<tam_a(arr);
+    int fila;
+    cout<<"ingrese numero de filas de la matriz: ";
+    cin>>fila;
+    int columna;
+    cout<<"ingrese numero de columna de la matriz: ";
+    cin>>columna;
+    int tem;
 
+    int mat_a[fila][columna];
+    for (int i = 0 ; i<fila;i++){
+        for (int j = 0 ; j < columna;j++){
+            cout<<"ingrese valor de la matriz: ";
+            cin>>tem;
+            mat_a[i][j]=tem;
+
+
+        }
+
+    }
+    int fil;
+    cout<<"ingrese numero de filas de la matriz: ";
+    cin>>fil;
+    int col;
+    cout<<"ingrese numero de columna de la matriz: ";
+    cin>>col;
+
+    int mat_b[fil][col];
+    for (int i = 0 ; i<fil;i++){
+        for (int j = 0 ; j < col;j++){
+            cout<<"ingrese valor de la matriz: ";
+            cin>>tem;
+            mat_b[i][j]=tem;
+
+
+        }
+
+    }
+
+
+    for (int i = 0 ; i<fila;i++){
+        for (int j = 0 ; j < columna;j++){
+            cout<< mat_a[i][j] <<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+
+    for (int i = 0 ; i<fil;i++){
+        for (int j = 0 ; j < col;j++){
+            cout<< mat_b[i][j] <<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+
+
+    mul_mat(mat_a,mat_b,fila,columna,fil,col);
+
+
+
+
+
+
+    /*char arr[] = "hello";
+    cout<<tam_a(arr);
+    */
     /*int tam;
     cout<<"ingresar el tamaño:"<<endl;
     cin>>tam;
@@ -138,12 +250,30 @@ int main()
     }
 
     for(int i =0 ; i < tam ;i++){
-        cout<<arreglo[i];
+        cout<<arreglo[i]<<" ";
 
     }
 
-    cout<<endl;*/
+    cout<<endl;
+    /*invert(arreglo,tam);
+    cout<<endl;
+    for(int i =0 ; i < tam ;i++){
+        cout<<arreglo[i];
 
+    }*/
+    /*ord_arr(arreglo,tam);
+
+    for(int i =0 ; i < tam ;i++){
+        cout<<arreglo[i]<<" ";
+
+    }
+    */
+    /*suma = sum(arreglo,tam);
+    pro=prom(arreglo,tam);
+    cout<<endl;
+    cout<<"la suma es:"<<suma<<endl;
+    cout<<"el promedio es:"<<pro<<endl;
+*/
 
     /*suma = sum(arreglo,tam);
     pro = prom(arreglo,tam);
@@ -153,20 +283,7 @@ int main()
 
     */
 
-    /*invert(arreglo,tam);
 
-
-    for(int i =0 ; i < tam ;i++){
-        cout<<arreglo[i];
-
-    }*/
-
-   /* ord_arr(arreglo,tam);
-
-    for(int i =0 ; i < tam ;i++){
-        cout<<arreglo[i];
-
-    }*/
 
    /* int prim;
     cout<<"ingresar el rando de numero primos"<<endl;
